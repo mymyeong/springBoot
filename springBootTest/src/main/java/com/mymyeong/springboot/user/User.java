2057f4cc2d26b0aff3cae97571f5df3de2ff424e
mjhan <mymyeong@gmail.com>
2021-11-26 16:06:47 +0900

    Spring Boot Test

---
+++ springBootTest/src/main/java/com/mymyeong/springboot/user/User.java
@@ -0,0 +1,15 @@
+package com.mymyeong.springboot.user;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+	private Integer id;
+	private String name;
+	private Date joinDate;
+
+}
