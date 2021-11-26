fb38de322ab952b22d0d64c2a08731ae2dbd2b9d
mjhan <mymyeong@gmail.com>
2021-11-26 18:05:11 +0900

    Exception

---
+++ SpringBootTest/src/main/java/com/mymyeong/springboot/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.mymyeong.springboot.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.mymyeong.springboot.user.UserNotFoundException;
+
+@RestController
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse er = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<Object>(er, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse er = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<Object>(er, HttpStatus.NOT_FOUND);
+	}
+}
