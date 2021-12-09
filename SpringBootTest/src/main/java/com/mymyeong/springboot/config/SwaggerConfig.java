8aaf81ac7b1dffcb57ba20d89c262e4553929d0c
mjhan <mymyeong@gmail.com>
2021-12-09 16:43:28 +0900

    swagger 오류 수정

--- SpringBootTest/src/main/java/com/mymyeong/springboot/config/SwaggerConfig.java
+++ SpringBootTest/src/main/java/com/mymyeong/springboot/config/SwaggerConfig.java
@@ -1,10 +1,17 @@
 package com.mymyeong.springboot.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,8 +20,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	private static final Contact DEFAULT_CONTACT = new Contact("mymyeong", "https://mymyeong.com", "mymyeong@gmail.com");
+	private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("title", "description", "version", "termsOfServiceUrl", DEFAULT_CONTACT, "license", "licenseUrl", new ArrayList<>());
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES)//
+				.select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
 	}
 }
